0aac369eaf000521faeb6b828d1c410b2372d064
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-10-09 17:10:47 +0100

    cors enabled (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,18 @@
-
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  // ✅ Allow your Netlify frontend to communicate with backend
   app.enableCors({
-    origin: [
-      '*',
-      //'http://localhost:5173',
-      'http://localhost:3000',
-      // 'https://unmask-frontend.vercel.app',
-      // 'https://unmask-frontend-git-dev-tolulope.vercel.app',
-      // 'https://unmask-frontend-git-main-tolulope.vercel.app',
-      
-    ], // Allow all origins
+    origin: ['https://magenta-granita-69bf83.netlify.app', 'http://localhost:3000'],
+    credentials: true,
   });
 
-  console.log('🌐 CORS enabled for all origins');
-  console.log('🔧 Global prefix set to /api');
-
-  await app.listen(process.env.PORT ?? 3000);
-
-  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
+  await app.listen(process.env.PORT ?? 5000);
+  console.log(`🚀 Server running on ${await app.getUrl()}`);
 }
-bootstrap().catch((err) => {
-  console.error('❌ Error starting server:', err);
-});
+bootstrap();
